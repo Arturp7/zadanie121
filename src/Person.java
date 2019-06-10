fd866072c1de311b3f5569998df6c67850c2321e
Artur Przyjemski <spinezpl@gmail.com>
2019-06-10 13:24:18 +0200

    first commit

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String pesel;
+
+
+    public Person(String firstName, String lastName, int age, String pesel) {
+        if (firstName.length() >= 2) {
+            this.firstName = firstName;
+        } else throw new NameUndefinedException();
+         if (lastName.length() >=  2)
+        this.lastName = lastName;
+         else throw new NameUndefinedException();
+        if (age >= 1)
+            this.age = age;
+        else throw new IncorrectAgeException();
+        this.pesel = pesel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+}
